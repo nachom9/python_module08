b33cc9edc35a75bebb72fd38eb52b21d6c947a3c
Ignacio Melero Carracedo <imelero-@c1r14s4.42madrid.com>
2026-02-19 18:22:19 +0100

    ex0

--- ex01/operator.py
+++ ex01/operator.py
@@ -1,11 +1,1 @@
 #!/usr/bin/env python3
-
-import pandas as pd
-
-# Crear un DataFrame
-data = {'Nombre': ['Ana', 'Luis', 'Carlos'],
-        'Edad': [23, 34, 45]}
-df = pd.DataFrame(data)
-
-# Imprimir el DataFrame
-print(df)
